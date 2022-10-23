90a52e44f576f768e2ad78be3eb5608723e88635
salonipatil <patil.sal@northeastern.edu>
2022-10-23 15:58:28 -0400

    A6- Deleted old Tuiter

--- public/tuiter/PostSummaryList/index.js
+++
@@ -1,7 +0,0 @@
-import PostSummaryList from "../PostSummaryList/PostSummaryList.js";
-$('#wd-todo').append(`
-   <div class="container">
-       <h1>List example</h1>
-       ${PostSummaryList()}
-   </div>
-`);
