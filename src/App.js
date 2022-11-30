1a710cd333a8b5de829bd0a544c37cd6e65fd0f7
salonipatil <patil.sal@northeastern.edu>
2022-11-30 17:18:35 -0500

    A8

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import Labs from "./labs";
-import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router-dom";
+import React from "react";
+
+
 
 function App() {
   return (
       <BrowserRouter>
           <div className="container">
               <Routes>
-                  <Route path="/*"
-                         element={<Labs/>}/>
-                  <Route path="/hello"
-                         element={<HelloWorld/>}/>
                   <Route path="/tuiter/*"
                          element={<Tuiter/>}/>
               </Routes>
--- src/nav.js
+++ src/nav.js
@@ -1,11 +1,8 @@
 import {Link} from "react-router-dom";
-
+import React from "react";
 function Nav() {
     return (
         <div>
-            <Link to="/">Labs</Link> |
-            <Link to="/">Assignment 6</Link> |
-            <Link to="/a7">Assignment 7</Link> |
             <Link to="/tuiter">Tuiter</Link>
 
         </div>
